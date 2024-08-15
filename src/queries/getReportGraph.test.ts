482556aea3cf561db63037771d2bf46739a654a9
WarcraftYax <joshyaxley93@gmail.com>
2024-08-15 18:15:06 +0100

    Re-enable fixed test

--- src/queries/getReportGraph.test.ts
+++ src/queries/getReportGraph.test.ts
@@ -1,11 +1,12 @@
+import { buildTestSdk } from '../testHelpers';
+
 test('returns data', async () => {
-  // FIXME: Re-enable when ViewType fix is live
-  // const response = await buildTestSdk().getReportGraph({
-  //   code: 'cFZ274xvNgTbLXCj',
-  //   encounterId: 2824,
-  //   filterExpression: 'ability.name in ("Lava Geysers")',
-  //   endTime: 2147483647,
-  // });
-  //
-  // expect(response?.reportData?.report).toMatchSnapshot();
+  const response = await buildTestSdk().getReportGraph({
+    code: 'cFZ274xvNgTbLXCj',
+    encounterId: 2824,
+    filterExpression: 'ability.name in ("Lava Geysers")',
+    endTime: 2147483647,
+  });
+
+  expect(response?.reportData?.report).toMatchSnapshot();
 });
